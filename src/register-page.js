130186becb93867f15fbe189dce01c5821c55626
Rahul Das <rahuldas@LPMB-Kol-Priyanjit.local>
2020-03-24 22:55:13 +0530

    user registration page done

--- src/register-page.js
+++ src/register-page.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import './register-page.css';
 const RegisterPage = ()=>{
     return (
-        <div>Successfull ;)</div>
+        <div className="register-page-container">
+            <div className="sub-container">
+                <label id="username">Username</label>
+                <input type="text"/>
+            </div>
+            <div className="sub-container">
+                <label id="password">Password</label>
+                <input type="text"/>
+            </div>
+            <div className="sub-container">
+                <button id="sign-in">
+                    SIGN IN
+                </button>
+            </div>
+        </div>
     );
 }
 export default RegisterPage;
--- src/register.js
+++ src/register.js
@@ -4,9 +4,6 @@ import './register.css';
 const Register = ()=>{
     return (
         <div className="register-container">
-            {/* <button id="register">
-                REGISTER
-            </button> */}
             <a href="http://localhost:3000/register">  
                  REGISTER
             </a>
