33136482d3384f59b607c3efdbc5f0d86806d100
Rahul Das <rahuldas@LPMB-Kol-Priyanjit.local>
2020-03-24 11:56:38 +0530

    set up express server

---
+++ src/routes/api.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+// get the list of users from the db
+router.get('/user', function (req, res, next){
+    res.send({
+        status: 'GET'
+    });
+});
+
+// add a user to the db
+router.post('/user', function (req, res, next){
+    res.send({
+        status: 'POST'
+    });
+});
+
+module.exports = router;
